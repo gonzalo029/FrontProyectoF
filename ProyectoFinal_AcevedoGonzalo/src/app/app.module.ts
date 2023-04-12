fa8bc891d829bc5026e147df2e89f8e7e219ec0a
josemaak <josemaak@gmail.com>
2023-04-12 01:00:26 -0300

    node

--- ProyectoFinal_AcevedoGonzalo/src/app/app.module.ts
+++ ProyectoFinal_AcevedoGonzalo/src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componente/header/header.component';
+import { LogoapComponent } from './componente/logoap/logoap.component';
+import { BannerComponent } from './componente/banner/banner.component';
+import { AcercaDeComponent } from './componente/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './componente/experiencia/experiencia.component';
+import { EducacionComponent } from './componente/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './componente/hys/hys.component';
+import { ProyectoComponent } from './componente/proyecto/proyecto.component';
+import { FooterComponent } from './componente/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoapComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HysComponent,
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
